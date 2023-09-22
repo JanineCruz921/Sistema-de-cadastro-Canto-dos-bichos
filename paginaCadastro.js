bc6d2977896841e4ace30c4111a9149b81c48a46
JanineCruz921 <cruzjanine921@gmail.com>
2023-09-22 12:14:45 -0300

    subindo os arquivos do projeto

--- paginaCadastro.js
+++ paginaCadastro.js
@@ -1,13 +1,8 @@
 var pets = [];
-var donos = [];
 
 const paginaIntroducao = document.querySelector("#paginaIntroducao")
 const paginaNovoCadastro = document.querySelector("#paginaNovoCadastro")
 const paginaCadastros = document.querySelector("#paginaCadastros")
-
-
-var btnNovoCadastro = document.querySelector("#btnNovoCadastro")
-var btnCadastros = document.querySelector("#btnCadastros")
 
 function trocaDePagina1() {
     paginaIntroducao.style.display = "none"
@@ -24,7 +19,7 @@ function trocaDePagina2() {
 
 
 class Pet {
-    constructor(id, nomePet, especie, raca, idade, peso, altura) {
+    constructor(id, nomePet, especie, raca, idade, peso, altura, nomeDono, contatoDono, editId) {
         this.id = id;
         this.nomePet = nomePet
         this.especie = especie;
@@ -32,41 +27,47 @@ class Pet {
         this.idade = idade;
         this.peso = peso;
         this.altura = altura;
+        this.nomeDono = nomeDono;
+        this.contatoDono = contatoDono;
+        this.editId = editId;
     }
 }
 
 
 function adicionarPet() {
+alert("Pet Cadastrado com sucesso, acesse VER PETS CADASTRADOS para ver o novo Pet")
     if (localStorage.petsArr) {
         pets = JSON.parse(localStorage.getItem(`petsArr`));
     }
     var id = pets.length + 1;
+    var editId = null;
     var nomePet = document.querySelector("#nomePet").value
     var especie = document.querySelector("#especiePet").value;
     var raca = document.querySelector("#racaPet").value;
     var idade = document.querySelector("#idadePet").value;
     var peso = document.querySelector("#pesoPet").value;
     var altura = document.querySelector("#alturaPet").value;
-    let novoPet = new Pet(id, nomePet, especie, raca, idade, peso, altura);
+    var nomeDono = document.querySelector("#nomeDono").value;
+    var contatoDono = document.querySelector("#contatoDono").value;
+
+    let novoPet = new Pet(id, nomePet, especie, raca, idade, peso, altura, nomeDono, contatoDono, editId);
     pets.push(novoPet)
 
+    localStorage.petsArr = JSON.stringify(pets);
+    limparInput()
+
+}
+
+function limparInput() {
     document.querySelector("#nomePet").value = "";
     document.querySelector("#especiePet").value = "";
     document.querySelector("#racaPet").value = "";
     document.querySelector("#idadePet").value = "";
     document.querySelector("#pesoPet").value = "";
     document.querySelector("#alturaPet").value = "";
-
-    console.log(pets)
-
-    localStorage.petsArr = JSON.stringify(pets);
+    document.querySelector("#nomeDono").value = "";
+    document.querySelector("#contatoDono").value = "";
 }
-
-//Essa parte do código tenta:
-// percorrer o banco de dados e exibir os dados armazenados
-//criar uma nova celula na tabela para adicionar o novo dado nela
-//criar botões referentes ao novo dado adicionado no final da nova celula
-//pra fazer essa parte do código assita o vídeo de adicionar dados de um array numa tabela
 
 function exibirPet() {
     if (localStorage.petsArr) {
@@ -81,6 +82,8 @@ function exibirPet() {
         let TdIdade = tr.insertCell();
         let TdPeso = tr.insertCell();
         let TdAltura = tr.insertCell();
+        let TdNomeDono = tr.insertCell();
+        let TdContatoDono = tr.insertCell();
         let Td_btns = tr.insertCell();
 
         TdNome.innerText = pets[i].nomePet;
@@ -89,66 +92,36 @@ function exibirPet() {
         TdIdade.innerText = pets[i].idade;
         TdPeso.innerText = pets[i].peso;
         TdAltura.innerText = pets[i].altura;
+        TdNomeDono.innerText = pets[i].nomeDono;
+        TdContatoDono.innerText = pets[i].contatoDono;
 
-        let novoBotaoDono = document.createElement("button");
-        novoBotaoDono.innerText = "Dados do Dono"
-        novoBotaoDono.addEventListener("click", exibirDono())
-        Td_btns.append(novoBotaoDono)
 
         let novaImgEditar = document.createElement("img");
-        novaImgEditar.src = "./assets/ícones/icone_edicao.png"
-        novaImgEditar.setAttribute("onclick", "editar("+pets[i].id+")")
-        Td_btns.append(novaImgEditar)
+        novaImgEditar.src = "./assets/ícones/icone_edicao.png";
+        novaImgEditar.setAttribute ("onclick", "prepararEdicao()")
+        Td_btns.append(novaImgEditar);
 
         let novaImgExcluir = document.createElement("img");
         novaImgExcluir.src = "./assets/ícones/icone_Exclusao.png";
-        novaImgExcluir.setAttribute("onclick", "deletar("+pets[i].id+");")
+        novaImgExcluir.setAttribute("onclick", "deletar(" + pets[i].id + ");")
         Td_btns.append(novaImgExcluir);
     }
-    
+
 }
 
 function deletar(id) {
-    //alert('deletando id '+ id)
-    for(let i= 0; i< pets.length; i++){
-        if(pets[i].id == id){
-            pets.splice(i, 1);
-            tBody.deleteRow(i);
-            localStorage.removeItem([i])
+    if (confirm("Deseja mesmo deletar este item")) {
+    for (let i = 0; i < pets.length; i++) {
+            if (pets[i].id == id) {
+                pets.splice(i, 1);
+                tBody.deleteRow(i);
+                localStorage.removeItem([i])
+            }
         }
         localStorage.petsArr = JSON.stringify(pets);
     }
 }
-function editar() {
-    alert("editando")
-}
 
-class Dono {
-    constructor(nomeDono, endereco, cep, contato, email, cpf) {
-        this.nomeDono = nomeDono
-        this.endereco = endereco
-        this.cep = cep
-        this.contato = contato
-        this.email = email
-        this.cpf = cpf
-    }
-}
 
-function adicionarDono() {
-    var nomeDono = document.querySelector("#nomeDono").value
-    var endereco = document.querySelector("#enderecoDono").value
-    var cep = document.querySelector("#cepDono").value
-    var contato = document.querySelector("#contatoDono").value
-    var email = document.querySelector("#emailDono").value
-    var cpf = document.querySelector("#cpfDono").value
-    let novoDono = new Dono(nomeDono, endereco, cep, contato, email, cpf);
-    donos.push(novoDono)
-    console.log(donos)
-}
-
-function exibirDono() {
-    var PopUpDono = document.querySelector(".divDadosDono")
-    PopUpDono.style.display = "flex"
-}
 
 
